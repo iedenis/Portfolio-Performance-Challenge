1380ed2431b909cf624240d4f3cf15c546a90d15
iedenis <denis.ievlev@gmail.com>
2019-11-20 02:31:57 +0200

    fixing deploying settings

---
+++ build/precache-manifest.53edcbeeca4f8dd8407c60320f4a4e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e43c32f9ff71c2d6347e27b7a0583ce",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7262e71223b6dea6e08a",
+    "url": "/portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9ae495eed8bf61e3ccd2",
+    "url": "/portfolio/static/css/main.af37d185.chunk.css"
+  },
+  {
+    "revision": "7262e71223b6dea6e08a",
+    "url": "/portfolio/static/js/2.ee06bf22.chunk.js"
+  },
+  {
+    "revision": "9ae495eed8bf61e3ccd2",
+    "url": "/portfolio/static/js/main.7c1a67a9.chunk.js"
+  },
+  {
+    "revision": "7f8073ac89f4a4f22a25",
+    "url": "/portfolio/static/js/runtime-main.2ee86687.js"
+  }
+]);
